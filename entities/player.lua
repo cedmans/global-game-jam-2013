0e9e864eb46bdc549dd15c6cdfd8bbb2c3fa3507
Marc Zych <marczych@gmail.com>
2013-01-26 00:08:12 -0800

    Basic player animation

--- entities/player.lua
+++ entities/player.lua
@@ -2,7 +2,16 @@ local Class = require "hump.class"
 local Vector = require "hump.vector"
 local Constants = require "constants"
 
-local playerImage = love.graphics.newImage("assets/images/avatarside_frame1.png")
+local playerImages = {
+   love.graphics.newImage("assets/images/avatarside_frame1.png"),
+   love.graphics.newImage("assets/images/avatarside_frame2.png"),
+   love.graphics.newImage("assets/images/avatarside_frame3.png"),
+   love.graphics.newImage("assets/images/avatarside_frame4.png"),
+   love.graphics.newImage("assets/images/avatarside_frame5.png"),
+   love.graphics.newImage("assets/images/avatarside_frame6.png"),
+   love.graphics.newImage("assets/images/avatarside_frame7.png"),
+   love.graphics.newImage("assets/images/avatarside_frame8.png")
+}
 
 local Player = Class(function(self)
    self.position = Vector(400, 400)
@@ -13,8 +22,10 @@ function Player:update(dt)
    self.position = self.position + (self.targetpos - self.position):normalized() * 100 * dt
 end
 
-function Player:draw(dt)
-   love.graphics.draw(playerImage, self.position.x - Constants.PLAYER_WIDTH/2,
+function Player:draw(time)
+   local image = playerImages[math.floor(time * 5) % 8 + 1]
+
+   love.graphics.draw(image, self.position.x - Constants.PLAYER_WIDTH/2,
     self.position.y - Constants.PLAYER_HEIGHT/2)
 end
 
