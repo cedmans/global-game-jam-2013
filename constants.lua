78a1351c9e75f16a41a3568ff997f07de35d8401
Marc Zych <marczych@gmail.com>
2013-01-25 23:04:26 -0800

    Use capital 'C' constants to be more consistent

--- constants.lua
+++ constants.lua
@@ -1,18 +1,18 @@
-local constants = {}
+local Constants = {}
 
-constants.SCREEN_WIDTH = 1024
-constants.SCREEN_HEIGHT = 720
+Constants.SCREEN_WIDTH = 1024
+Constants.SCREEN_HEIGHT = 720
 
-constants.MIN_X = 0
-constants.MAX_X = constants.SCREEN_WIDTH
-constants.MIN_Y = 0
-constants.MAX_Y = constants.SCREEN_HEIGHT
+Constants.MIN_X = 0
+Constants.MAX_X = Constants.SCREEN_WIDTH
+Constants.MIN_Y = 0
+Constants.MAX_Y = Constants.SCREEN_HEIGHT
 
-constants.SADDIE_HEALTH_REDUCTION = -5 -- Units per second
+Constants.SADDIE_HEALTH_REDUCTION = -5 -- Units per second
 
-constants.SADNESS_BAR_OFFSET = 20
-constants.WARNING_SADNESS = 30
-constants.CRITICAL_SADNESS = 10
-constants.SADNESS_ALERT_OFFSET = 18
+Constants.SADNESS_BAR_OFFSET = 20
+Constants.WARNING_SADNESS = 30
+Constants.CRITICAL_SADNESS = 10
+Constants.SADNESS_ALERT_OFFSET = 18
 
-return constants
+return Constants
