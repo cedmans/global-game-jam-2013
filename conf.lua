8f572ce055eba718e50813f8170c58847d4efcbf
Marc Zych <marczych@gmail.com>
2013-01-25 21:30:00 -0800

    Move screen size into constants

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 -- https://love2d.org/wiki/Config_Files
 
+local Constants = require "constants"
+
 function love.conf(t)
-   t.screen.width = 1024
-   t.screen.height = 720
+   t.screen.width = Constants.SCREEN_WIDTH
+   t.screen.height = Constants.SCREEN_HEIGHT
 end
-
-
--- constants.lua
+++ constants.lua
@@ -1,8 +1,11 @@
 local constants = {}
 
+constants.SCREEN_WIDTH = 1024
+constants.SCREEN_HEIGHT = 720
+
 constants.MIN_X = 0
-constants.MAX_X = 1024
+constants.MAX_X = constants.SCREEN_WIDTH
 constants.MIN_Y = 0
-constants.MAX_Y = 728
+constants.MAX_Y = constants.SCREEN_HEIGHT
 
 return constants
