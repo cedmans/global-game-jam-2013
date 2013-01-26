99e82db141dc1652bfd781df8c826cc60826bc89
Marc Zych <marczych@gmail.com>
2013-01-25 19:20:42 -0800

    Base lua with love program
    Just a screen with an incrementing counter.

--- main.lua
+++ main.lua
@@ -1,4 +1,13 @@
-function love.draw()
-   love.graphics.print("Global Game Jam!", 400, 300)
+local counter = 0
+
+function love.init()
+   counter = 0
 end
 
+function love.update()
+   counter = (counter + 1) % 100
+end
+
+function love.draw()
+   love.graphics.print(counter, 400, 300)
+end
