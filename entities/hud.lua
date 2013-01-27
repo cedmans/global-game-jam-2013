c4f24fa7c544b12364d20e2ad1041775c3a76c53
LeJon McGowan <lejonmcgowan@gmail.com>
2013-01-26 17:39:08 -0800

    score refinement/hud improvement

--- entities/hud.lua
+++ entities/hud.lua
@@ -6,34 +6,51 @@ local Util = require "util"
 local originalFont = love.graphics.newFont(18)
 local scoreFont = love.graphics.newFont("assets/fonts/arialbd.ttf", 18)
 local endFont = love.graphics.newFont("assets/fonts/pixel.ttf", 30)
-score = 0  
+local r,g,b,a = love.graphics.getColor()
+score = 0
+sadCount = 5
 local Hud = Class(function(self)
     
 end)
 
 function Hud:draw(time)  
+   love.graphics.setColor(0,150,0)
+   
    love.graphics.setFont(scoreFont)
-   score = score + (3*#(saddies))
+   score = score + (3*sadCount)
    love.graphics.setFont(originalFont)
    love.graphics.print("SCORE: " .. score, 870,20)
    
+   
    love.graphics.print("TIME: " .. math.floor(time), 50, 50)
    love.graphics.print("Lives: " .. math.floor(lives), 50, 70)
+   
+   love.graphics.setColor(r,g,b,a)
+   
+   
 end
 
 function Hud:getScore()
    return score
 end
 
+function Hud:sadIncrement(i)
+   sadCount = sadCount + i
+end
+
 
 function Hud:endDisplay(time)
-   love.graphics.setFont(endFont)
+   love.graphics.setColor(0,150,0)
+  
+  love.graphics.setFont(endFont)
    love.graphics.print("Game Over",Constants.SCREEN_WIDTH / 2,Constants.SCREEN_HEIGHT / 2)
    
    love.graphics.setFont(scoreFont)
    love.graphics.print("FINAL SCORE: " .. self:getScore(),770,20)
    love.graphics.setFont(originalFont)
-   love.graphics.print("Yout kept the world happy for  " .. math.floor(time) .. " seconds", 50, 40)
+   love.graphics.print("Yout kept up to " .. sadCount .. " people happy for " .. math.floor(time) .. " seconds", 40, 20)
+   
+   love.graphics.setColor(r,g,b,a)
          
 end 
 
