d9a3d36fff67123311d2fc8dc3fdc4be67cb289a
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-01-26 16:59:42 -0800

    Use 0->1 scale for DeadSaddie death progress.

--- entities/deadsaddie.lua
+++ entities/deadsaddie.lua
@@ -5,18 +5,18 @@ local Constants = require "constants"
 local DeadSaddie = Class(function(self, saddie)
    self.image = saddie.image
    self.position = saddie.position
-   self.progress = 100
+   self.progress = 1
 end)
 
 function DeadSaddie:update(dt)
-   self.progress = self.progress - 1
+   self.progress = self.progress - .01
 end
 
 function DeadSaddie:draw(time)
    -- Store colors for later resetting.
    r, g, b, a = love.graphics.getColor()
 
-   love.graphics.setColor(r, g, b, self.progress / 100 * 255)
+   love.graphics.setColor(r, g, b, self.progress * 255)
    love.graphics.draw(self.image, self.position.x - Constants.SADDIE_WIDTH/2,
     self.position.y - Constants.SADDIE_HEIGHT/2)
 
@@ -24,7 +24,7 @@ function DeadSaddie:draw(time)
 end
 
 function DeadSaddie:finishedDying()
-   return self.progress == 0
+   return self.progress <= 0
 end
 
 return DeadSaddie
