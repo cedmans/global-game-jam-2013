e98923013369c5511f8c4b32d174440730543943
LeJon McGowan <lejonmcgowan@gmail.com>
2013-01-26 23:04:26 -0800

    counter upgraded

--- main.lua
+++ main.lua
@@ -11,8 +11,4 @@ function love.load()
    -- Register the game state dispatcher and switch into the initial state.
    Gamestate.registerEvents()
    Gamestate.switch(title)
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> a7d1f6b05a16ca73fdccc4a2eac621f35fa58712
