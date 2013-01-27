3543388768e3f43e27b8228f58710cf55ffae268
Cedric Wienold <cwienold@gmail.com>
2013-01-27 13:10:33 -0800

    Remove text numbers for items

--- entities/toolbar.lua
+++ entities/toolbar.lua
@@ -17,17 +17,14 @@ function Toolbar:draw()
 
    love.graphics.draw(toolURIs["mouth"], self.position.x, self.position.y)
    love.graphics.setColor(0,102,0,255)
-   love.graphics.printf("1", self.position.x, self.position.y + 3, Constants.TOOLBAR_ITEM_WIDTH, 'center')
    love.graphics.setColor(oldr,oldg,oldb,olda)
 
    love.graphics.draw(toolURIs["wave"], self.position.x+Constants.TOOLBAR_ITEM_WIDTH, self.position.y)
    love.graphics.setColor(0,102,0,255)
-   love.graphics.printf("2", self.position.x + Constants.TOOLBAR_ITEM_WIDTH, self.position.y + 3, Constants.TOOLBAR_ITEM_WIDTH, 'center')
    love.graphics.setColor(oldr,oldg,oldb,olda)
 
    love.graphics.draw(toolURIs["lovepotion"], self.position.x+Constants.TOOLBAR_ITEM_WIDTH*2, self.position.y)
    love.graphics.setColor(0,102,0,255)
-   love.graphics.printf("3", self.position.x + Constants.TOOLBAR_ITEM_WIDTH*2, self.position.y + 3, Constants.TOOLBAR_ITEM_WIDTH, 'center')
    love.graphics.setColor(oldr,oldg,oldb,olda)
 end
 
