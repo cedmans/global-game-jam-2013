d02224164f398213725b2dc2345d3cb04a12ffab
Cedric Wienold <cwienold@gmail.com>
2013-01-27 16:07:26 -0800

    Output bin to current dir

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-zip -r ../${PWD##*/}.love *
-cat /usr/bin/love "../global-game-jam-2013.love" > ../heart-healer-extreme && chmod +x ../heart-healer-extreme
+zip -r ./${PWD##*/}.love *
+cat /usr/bin/love "./global-game-jam-2013.love" > ./heart-healer-extreme && rm "./global-game-jam-2013.love" && chmod +x ./heart-healer-extreme
 
